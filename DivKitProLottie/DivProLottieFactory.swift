c7290105d868afcd758e4baa3fb003329ad9da97
Nina Popova <ventywing@yandex-team.ru>
2023-08-22 11:39:38 +0300

    5.1.0 ver

---
+++ DivKitProLottie/DivProLottieFactory.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+final public class DivProLottieFactory {
+  public static let version = "5.1.0"
+  public init() {}
+  public func make() {}
+}
--- Package.swift
+++ Package.swift
@@ -1,34 +1,41 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
-  name: "DivKitPro",
-  platforms: [
-    .iOS(.v11),
-  ],
-  products: [
-    .library(name: "DivKitPro", targets: ["WrapperForDivKitPro"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.2.0"),
-    .package(url: "https://github.com/divkit/divkit-ios.git", from: "27.0.0"),
-  ],
-  targets: [
-    .binaryTarget(
-      name: "DivKitPro",
-      url: "https://div-storage.s3.yandex.net/releases/DivKitPro-2.0.1.xcframework.zip",
-      checksum: "9bea5261ff9e3a8d80a51947e6a2f34a5153ae81380f63144b1438e5fb76c663"
-    ),
-    .target(
-      name: "WrapperForDivKitPro",
-      dependencies: [
-        .target(name: "DivKitPro"),
-        .product(name: "DivKit", package: "divkit-ios"),
-        .product(name: "DivKitExtensions", package: "divkit-ios"),
-        .product(name: "Lottie", package: "lottie-ios"),
+    name: "DivKitPro",
+    platforms: [
+        .iOS(.v11),
       ],
-      path: "wrapper-library-ios"
-    ),
-  ]
+    products: [
+        .library(
+            name: "DivKitPro",
+            targets: ["DivKitPro"]),
+        .library(
+            name: "DivKitProLottie",
+            targets: ["DivKitProLottie"]),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/divkit/divkit-ios.git", from: "27.0.0"),
+      .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.2.0"),
+    ],
+    targets: [
+        .target(
+            name: "DivKitPro",
+            dependencies: [
+              .product(name: "DivKit", package: "divkit-ios"),
+              .product(name: "DivKitExtensions", package: "divkit-ios"),
+            ],
+            path: "DivKitPro"
+        ),
+        .target(
+            name: "DivKitProLottie",
+            dependencies: [
+              .product(name: "DivKit", package: "divkit-ios"),
+              .product(name: "DivKitExtensions", package: "divkit-ios"),
+              .product(name: "Lottie", package: "lottie-ios"),
+            ],
+            path: "DivKitProLottie"
+        ),
+    ]
 )
