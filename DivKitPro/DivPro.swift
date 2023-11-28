b9f55f78198c8ef11d7e092da03ef0e8cd67e59c
Nina Popova <ventywing@yandex-team.ru>
2023-11-28 18:06:49 +0300

    5.4.0

--- DivKitPro/DivPro.swift
+++ DivKitPro/DivPro.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 final public class DivPro {
-  public static let version = "5.3.0"
+  public static let version = "5.4.0"
   public init() {}
   public func onStart() {}
 }
--- DivKitProLottie/DivProLottieFactory.swift
+++ DivKitProLottie/DivProLottieFactory.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 final public class DivProLottieFactory {
-  public static let version = "5.1.0"
+  public static let version = "5.4.0"
   public init() {}
   public func make() {}
 }
